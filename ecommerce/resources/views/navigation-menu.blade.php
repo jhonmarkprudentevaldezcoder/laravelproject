c9f84d60b5aafcf53654a25e997b68cb0998a7d8
JHON MARK PRUDENTE VALDEZ <jhonmarkprudentevaldezcoder@gmail.com>
2023-08-20 01:35:07 +0800

    update

--- ecommerce/resources/views/navigation-menu.blade.php
+++ ecommerce/resources/views/navigation-menu.blade.php
@@ -151,6 +151,7 @@
             <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
         </div>
 
         <!-- Responsive Settings Options -->
